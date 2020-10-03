6c1e805461fa5ec50cb1bef61d5052ff732b8a86
Shubhashish Verma <svashish305@gmail.com>
2020-10-04 04:33:11 +0530

    minor fixes

--- backend/middleware/authorize.js
+++ backend/middleware/authorize.js
@@ -1,28 +1,29 @@
-const jwt = require('express-jwt');
-const { secret } = require('../config.json');
-const db = require('../db');
+const jwt = require("express-jwt");
+const { secret } = require("../config.json");
+const db = require("../db");
 
 module.exports = authorize;
 
 function authorize(roles = []) {
-    if (typeof roles === 'string') {
-        roles = [roles];
-    }
+  if (typeof roles === "string") {
+    roles = [roles];
+  }
 
-    return [
-        jwt({ secret, algorithms: ['HS256'] }),
+  return [
+    jwt({ secret, algorithms: ["HS256"] }),
 
-        async (req, res, next) => {
-            const user = await db.User.findById(req.user.id);
-            const refreshTokens = await db.RefreshToken.find({ user: user.id });
+    async (req, res, next) => {
+      const user = await db.User.findById(req.user.id);
+      const refreshTokens = await db.RefreshToken.find({ user: user._id });
 
-            if (!user || (roles.length && !roles.includes(user.role))) {
-                return res.status(401).json({ message: 'Unauthorized' });
-            }
+      if (!user || (roles.length && !roles.includes(user.role))) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
 
-            req.user.role = user.role;
-            req.user.ownsToken = token => !!refreshTokens.find(x => x.token === token);
-            next();
-        }
-    ];
+      req.user.role = user.role;
+      req.user.ownsToken = (token) =>
+        !!refreshTokens.find((x) => x.token === token);
+      next();
+    },
+  ];
 }
