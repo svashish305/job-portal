9d857679ea38950f4e37edeb53f530c79d4b33c2
Shubhashish Verma <svashish305@gmail.com>
2020-10-04 04:59:45 +0530

    added toasts

--- frontend/src/components/candidate-job.list.js
+++ frontend/src/components/candidate-job.list.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { API } from "../api-service";
+import { Row, Col, Toast } from "react-bootstrap";
 
 function CandidateJobList(props) {
   const [token] = useCookies(["jp-token"]);
+  const [show, setShow] = useState(false);
   const [applyState, setApplyState] = useState([]);
 
   useEffect(() => {
@@ -28,7 +30,7 @@ function CandidateJobList(props) {
     if (jobIds.length) {
       // console.log(jobIds);
       API.applyForJob(JSON.stringify(jobIds), token["jp-token"])
-        .then((resp) => console.log(resp))
+        .then((resp) => setShow(true))
         .catch((error) => console.log(error));
     }
   };
@@ -40,9 +42,9 @@ function CandidateJobList(props) {
         <div key={j._id}>
           <label>
             {j.company} - {j.desc} -{" "}
-            {j.applicants && j.applicants.includes(props.loggedInUser._id)
+            {/* {j.applicants && j.applicants.includes(props.loggedInUser._id)
               ? "Applied"
-              : "Not Applied"}
+              : "Not Applied"} */}
           </label>
           <input
             onChange={(event) => {
@@ -64,6 +66,29 @@ function CandidateJobList(props) {
       <button type="submit" className="btn btn-primary" onClick={selectJobs()}>
         Apply
       </button>
+      <div className="toast-container">
+        <Row>
+          <Col xs={6}>
+            <Toast
+              onClose={() => setShow(false)}
+              show={show}
+              delay={3000}
+              autohide
+            >
+              <Toast.Header>
+                <img
+                  src="holder.js/20x20?text=%20"
+                  className="rounded mr-2"
+                  alt=""
+                />
+                <strong className="mr-auto">MERN JOB PORTAL</strong>
+                <small>A few seconds ago</small>
+              </Toast.Header>
+              <Toast.Body>Woohoo, you've applied to multiple jobs!</Toast.Body>
+            </Toast>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
