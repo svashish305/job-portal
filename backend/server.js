a3eb1dfedd690d054356feff606ce946277e1a55
Shubhashish Verma <svashish305@gmail.com>
2020-10-04 13:59:51 +0530

    minor bug fix

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  // });
 }
 
 const port =
