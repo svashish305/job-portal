b79a5e7fc217a790b5c01c11a6991ff3aa42eca9
Shubhashish Verma <svashish305@gmail.com>
2020-10-04 14:57:30 +0530

    updated backend files

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if (
 ) {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+    res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
   });
 }
 
