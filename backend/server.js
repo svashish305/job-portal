ccaad2ce727033ba63c5fd59d9ca31c004dc41da
Shubhashish Verma <svashish305@gmail.com>
2020-10-04 14:52:05 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
   });
 }
